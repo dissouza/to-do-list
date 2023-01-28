caa51a8e28414e90539b45b567f8fd950ef1bb79
Diego Souza <diego-santos14@live.com>
2023-01-27 23:25:00 -0300

    Incluida função exluir

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,5 +3,11 @@ function adicionar(){
 	let tarefa = getElementByClassName("task").text.value;
 	let hora = getElementById("hora").value;
 	let data = getElementById("data").value;
+
+
+	
+}
+
+function excluir(){
 	
 }
