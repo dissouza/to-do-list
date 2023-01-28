db4fc061e2c052c7ac09308e5e577c865242d18e
Diego Souza <diego-santos14@live.com>
2023-01-28 00:53:21 -0300

    Adicionada função para inserir nova tarefa

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,13 +1,15 @@
 function adicionar(){ 
 
-	let tarefa = getElementByClassName("task").text.value;
-	let hora = getElementById("hora").value;
-	let data = getElementById("data").value;
-
-
+	let tarefa = document.getElementById('task').value;
+	let hora = document.getElementById('hora').value;
+	let data = document.getElementById('data').value;
 	
+	let list_tasks = document.getElementById("list_tasks");
+	list_tasks.innerHTML = "<label class='new_task'><p id='name_task'>" + tarefa + "</p>" + "<p id='hour_task'>" + hora + "</p>" + "<p id='date_task'>" + data +"</p>" + "</label>" + "<section><button onclick='excluir()' id='excluir'>X</button></section>";
 }
 
+
+
 function excluir(){
-	
+
 }
