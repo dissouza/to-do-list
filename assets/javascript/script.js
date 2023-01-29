2314e03f0d868aa483bfa1a88f9f6509f68d33f1
Diego Souza <diego-santos14@live.com>
2023-01-29 13:13:53 -0300

    Adicionado condição de preenchimento dos campos

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,39 +1,33 @@
-let tarefa = document.getElementById('task').value;
-let hora = document.getElementById('hora').value;
-let data = document.getElementById('data').value;
+function adicionar(){
 
-function adicionar(){	
+	let tarefa = document.getElementById('task').value;
+	let hora = document.getElementById('hora').value;
+	let data = document.getElementById('data').value;
 	
 	let newTask = document.createElement('label')
 	newTask.classList.add('new_task');
 	newTask.innerHTML =`
 	<input type='checkbox' id='check'>
-	<p id='name_task'></p>
-	<p id='hour_task'></p>
-	<p id='date_task'></p>
+	<p id='name_task'>${tarefa}</p>
+	<p id='hour_task'>${hora}</p>
+	<p id='date_task'>${data}</p>
 	</input>
-	</label>	
-	`;
+	</label>`;
 
 	let excluir_button = document.createElement('section')
 	excluir_button.classList.add('excluir_button');
 	excluir_button.innerHTML = `<button onclick='excluir()' id='excluir'>X</button>`;
 
-	if (tarefa && hora && data == 0){
-		alert("Preencha os dados da tarefa")
+	if (tarefa, hora, data == false){
+		alert("Preencha os dados da tarefa");
+	}
 
-		else{
+	else{
 
-
-
-			document.getElementById('list_tasks').appendChild(newTask);
-			document.getElementById('list_tasks').appendChild(excluir_button);
-
-		}
+	document.getElementById('list_tasks').appendChild(newTask);
+	document.getElementById('list_tasks').appendChild(excluir_button);
 	}
 }
-
-
 
 function excluir(){
 
