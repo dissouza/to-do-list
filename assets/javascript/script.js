a0ef94ec25f54819cdb83af536cace880be81d9d
Diego Souza <diego-santos14@live.com>
2023-01-30 23:16:28 -0300

    Alterado função excluir

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,29 +6,30 @@ function adicionar(){
 	
 	let newTask = document.createElement('label')
 	newTask.classList.add('new_task');
-	newTask.innerHTML =`
-	<input type='checkbox' id='check'>
-	<p id='name_task'>${tarefa}</p>
-	<p id='hour_task'>${hora}</p>
-	<p id='date_task'>${data}</p>
-	</input>
-	</label>`;
+	newTask.innerHTML =`<input type='checkbox' id='check'>
+						<p id='name_task'>${tarefa}</p>
+						<p id='hour_task'>${hora}</p>
+						<p id='date_task'>${data}</p>
+						</input>
+						</label>`;
 
 	let excluir_button = document.createElement('section')
 	excluir_button.classList.add('excluir_button');
 	excluir_button.innerHTML = `<input type='button' onclick='excluir()' id='excluir' value='X'></input>`;
 
 	if (tarefa, hora, data == false){
-		alert("Preencha os dados da tarefa");
+		alert("Preencha as informações da tarefa para adicionar");
 	}
 
 	else{
 
 	document.getElementById('list_tasks').appendChild(newTask);
 	document.getElementById('list_tasks').appendChild(excluir_button);
+
 	}
 }
 
 function excluir(){
-
+	const new_task = document.getElementByClassName(new_task);
+	document.getElementById("list_tasks").removeChild(new_task);
 }
